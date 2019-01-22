91c945ae87cdcc433fa024e5145660043c738fd3
Krystian Niedźwiedzki <krystian.niedzwiedzki@capgemini.com>
2019-01-22 14:46:40 +0100

    Commit v3

--- samples/core/src/main/resources/db/migration/h2/V0001__R001_Create_schema.sql
+++ samples/core/src/main/resources/db/migration/h2/V0001__R001_Create_schema.sql
@@ -149,3 +149,20 @@ CREATE TABLE Special(
     CONSTRAINT PK_Special PRIMARY KEY(id),
     CONSTRAINT UC_Special_name UNIQUE(name)
 );
+
+CREATE TABLE Supplier(
+    id BIGINT NOT NULL,
+    modificationCounter INTEGER NOT NULL,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    rate INTEGER,
+    CONSTRAINT PK_Supplier PRIMARY KEY(id),
+    CONSTRAINT UC_Supplier_name UNIQUE(name)
+);
+
+INSERT INTO Supplier (id, modificationCounter, name, description, rate) VALUES (1, 0, 'Natural Fruit Exports', 'Fruit', 4);
+
+INSERT INTO Supplier (id, modificationCounter, name, description, rate) VALUES (2, 0, 'CPS Gourmet', 'Meat', 5);
+
+INSERT INTO Supplier (id, modificationCounter, name, description, rate) VALUES (3, 0, 'Albafrost', 'Vegetables', 3);
+
